c2619773b6969d861c81496e40b8672da8331499
Guru <mail@gururaja.in>
2025-07-21 00:06:41 +0530

    zoho

---
+++ src/zoho/chennai/Q2.java
@@ -0,0 +1,19 @@
+package zoho.chennai;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int[][]arr=new int[][]{{1,2,3,4},{1,9,1,2},{1,0,0,1},{0,0,0,1}};
+        int r=arr.length;
+        int c=arr[0].length;
+        int[] ans=new int[r+c-1];
+        for (int i=0;i< r;i++) {
+            for(int j=0;j<c;j++) {
+                ans[i+j]+=arr[i][j];
+            }
+        }
+        // System.out.println(Arrays.toString(ans));
+        for (int an : ans) {
+            System.out.print(an + ",");
+        }
+    }
+}
